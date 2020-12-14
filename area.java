818990ae360ade9d2687764a642820993d11d954
Nermain Taibah <74972181+Nermaintaibah@users.noreply.github.com>
2020-12-14 15:34:24 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,21 @@
+class Room
+{
+  float length;
+  float breadth;
+  void getdata(float a,float b)
+{
+length = a;
+breadth = b;
+}
+}
+class Roomarea
+{
+  public static void main(string args[])
+{
+  float area;
+  Room room1 = new Room();
+  room1.getdata(14,20);
+  area = room2, length * room1.breadth;
+  system out.println("areas" + area);
+}
+}
